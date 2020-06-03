6e069ab8573a9dd3040d60a1c10a97c85c68898b
Xiaona Zhou <XiaonaZhouzxn@gmail.com>
2020-06-02 21:52:08 -0400

    fixed set seed problem with shuf

--- week1/gen_pair.sh
+++ week1/gen_pair.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+#echo "Enter Seed"
+#read SEED
+SEED=`date +%N|sed s/...$//`
+echo $SEED
 # r1 is a random number between 1 and 28
 # r1 is the location of the first pair 
-r1=$(($(shuf -i 1-28 -n 1 )))
+r1=$(($(shuf -i 1-28 -n 1  --random-source=<(echo $SEED))))
 
 # use r1 to find p1
 p1=$(($(awk 'NR=='$r1'{print $1}' pairs.txt)))
@@ -16,7 +20,7 @@ l1=$(($(awk '{temp+=1} END{print temp}' pairs2.txt)))
 # r2 is a random number between 1 and l1
 # r2 is the location of the second pair
 
-r2=$(($(shuf -i 1-$l1 -n 1 )))
+r2=$(($(shuf -i 1-$l1 -n 1  --random-source=<(echo $SEED))))
 
 # use r2 to find p2
 p2=$(($(awk 'NR=='$r2'{print $1}' pairs2.txt)))
@@ -24,13 +28,15 @@ p2=$(($(awk 'NR=='$r2'{print $1}' pairs2.txt)))
 # pair3.txt contains all useable pairs after selecting the second pair
 grep [$p2] -v  pairs2.txt > pairs3.txt
 l2=$(($(awk '{temp+=1} END{print temp}' pairs3.txt)))
-r3=$(($(shuf -i 1-$l2 -n 1 )))
+r3=$(($(shuf -i 1-$l2 -n 1  --random-source=<(echo $SEED))))
 
 p3=$(($(awk 'NR=='$r3'{print $1}' pairs3.txt)))
 
 
 p4=$(($(grep [$p3] -v  pairs3.txt)))
-
+echo "random"
+echo $r1 $r2 $r3
+echo "pairs"
 echo $p1 $p2 $p3 $p4
 
 
--- week1/tryshuf.sh
+++ week1/tryshuf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-get_fixed_random()
-{
-  openssl enc -aes-256-ctr -pass pass:"$1" -nosalt </dev/zero 2>/dev/null
-}
+#get_fixed_random()
+#{
+ # openssl enc -aes-256-ctr -pass pass:"$1" -nosalt </dev/zero 2>/dev/null
+#}
 
-
-shuf -i 1-28 -n 1 --random-source=<(get_fixed_random 1)
+read SEED
+shuf -i 1-28 -n 1 --random-source=<(echo $SEED)
